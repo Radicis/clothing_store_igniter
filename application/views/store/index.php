c6217578a7f9aa47de798b1acf4c5d4b2e93ef49
radicis <alloyddesign@gmail.com>
2015-10-13 21:13:23 +0100

    updated theme

--- application/views/store/index.php
+++ application/views/store/index.php
@@ -1,6 +1,3 @@
-
-<div class="row">
-    <div class="col-md-3">
         <p class="lead">Filters</p>
         <div class="list-group">
             <ul class="category-select">
@@ -39,43 +36,24 @@
                 echo "<li>" . anchor('store/filter/1/2', 'Test it') . "</li>";
                 ?>
             </ul>
-        </div>
-    </div>
-    <div class="col-md-9">
-       <div class="item-view">
-        <?php
+ </div>
 
-        if($items!=null) {
 
-            foreach ($items as $item) {
-                echo "<div class='col-sm-4 col-lg-4 col-md-4' >";
-                echo "<div class='thumbnail' >";
-                echo "<img src = 'http://placehold.it/320x150' alt = '' >";
-                echo "<div class='caption' >";
-                echo "<h4 class='pull-right' >";
-                echo "$" . $item->item_price . "</h4>";
-                echo anchor('item/view/' . $item->id, $item->item_name);
-                echo "</h4 >";
-                echo "<p>" . $item->item_description . "</p></div>";
-                echo "<div class='ratings'><p>";
-                for ($counter = 0; $counter < $item->rating; $counter++) {
-                    echo "<span class='glyphicon glyphicon-star' ></span>";
-                }
-                if ($this->session->is_logged_in) {
-                    echo "<span class='rating-icons'>";
-                    echo anchor('item/add_rating/' . $item->id, "<i class='rate fa fa-thumbs-up'></i>");
-                    echo anchor('item/remove_rating/' . $item->id, "<i class='rate fa fa-thumbs-down'></i>");
-                    echo "</span>";
-                }
-                echo "</p ></div ></div ></div>";
-            }
-        }
 
-        echo "<div class='pagination'>" . $links . "</div>";
-
-        ?>
-
-</div>
+        <!-- start grids_of_3 -->
+        <div class="grids_of_3">
+            <?php if($items!=null){
+                foreach($items as $item) { ?>
+                <div class="grid1_of_3">
+                    <a href="<?php echo 'item/view/'.$item->id ?>">
+                        <img src="<?php echo base_url() . "images/clothes/" . $item->image_large; ?>"  alt=""/>
+                        <h3><?php echo $item->item_name ?></h3>
+                        <span class="price">&euro;<?php echo $item->item_price ?></span>
+                    </a>
+                </div>
+            <?php }} ?>
+            <div class="clear"></div>
+            </div>
 
 
 
