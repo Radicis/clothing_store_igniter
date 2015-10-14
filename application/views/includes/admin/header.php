e9540928bd80e6bc99d878bbdc6b9736ab50d637
radicis <alloyddesign@gmail.com>
2015-10-14 14:50:47 +0100

    off canvas menu

--- application/views/includes/admin/header.php
+++ application/views/includes/admin/header.php
@@ -113,7 +113,17 @@
         <div id="smart_nav">
             <a class="navicon" href="#menu-left"> </a>
         </div>
-
+        <nav id="menu-left">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="products.html">products</a></li>
+                <li><a href="about.html">about</a></li>
+                <li><a href="index.html">pages</a></li>
+                <li><a href="blog.html">blog</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <div class="clear"></div>
+            </ul>
+        </nav>
         <div class="header_right">
             <ul>
                 <li><a href="#"><i  class="art"></i><span class="color1">30</span></a></li>
--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -104,6 +104,17 @@
         <div id="smart_nav">
             <a class="navicon" href="#menu-left"> </a>
         </div>
+        <nav id="menu-left">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="products.html">products</a></li>
+                <li><a href="about.html">about</a></li>
+                <li><a href="index.html">pages</a></li>
+                <li><a href="blog.html">blog</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <div class="clear"></div>
+            </ul>
+        </nav>
 
         <div class="header_right">
             <ul>
